bd885e828e4c00f688e62b070c63cf94e58a1e40
rakibulhasan65 <rakibulhasan392364@gmail.com>
2023-02-19 22:45:24 +0600

    Mail Send Contact Form

--- My-Portfolio/app/Http/Controllers/Frontend/ContactController.php
+++ My-Portfolio/app/Http/Controllers/Frontend/ContactController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Frontend\ContactMail;
+use App\Mail\PortfolioContactMail;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -27,6 +29,15 @@ class ContactController extends Controller
         $contactMailStore->message = $request->message;
         $contactMailStore->time = now();
         $contactMailStore->save();
+
+        Mail::to($request->email)->send();
+        if (Mail::failures()) {
+            return response()->Fail('Sorry! Please try again latter');
+        } else {
+            return response()->success('Successfully Send Your Mail!');
+        }
+
+
         $notification = array(
             'message' => 'Successfully Send Your Messages!',
             'alert-type' => 'info'
